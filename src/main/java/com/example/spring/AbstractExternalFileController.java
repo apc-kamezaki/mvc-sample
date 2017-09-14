3aa0745526330ffb7b1c6b42462fb9dfbb0d347c
Hitoshi Kamezaki <kamezaki@ap-com.co.jp>
2017-09-14 19:41:24 +0900

    organize import

--- src/main/java/com/example/spring/AbstractExternalFileController.java
+++ src/main/java/com/example/spring/AbstractExternalFileController.java
@@ -3,14 +3,12 @@ package com.example.spring;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Locale;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.MediaType;
 import org.springframework.util.AntPathMatcher;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
