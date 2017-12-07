0a8387c542ae7aadcf6fde8046c4cc62fbacd688
Hitoshi Kamezaki <kamezaki@ap-com.co.jp>
2017-12-07 14:25:13 +0900

    add pc|sp switch for ContentsController

--- src/main/java/com/example/spring/ContentsController.java
+++ src/main/java/com/example/spring/ContentsController.java
@@ -23,7 +23,8 @@ import com.example.spring.exceptions.ExternalFileNotFoundException;
 @RequestMapping(value = "/contents")
 public class ContentsController extends AbstractExternalFileController<SessionValueObject> {
 	private static final AntPathMatcher apm = new AntPathMatcher();
-	private static final Pattern subPathPattern = Pattern.compile("^(\\/contents\\/\\/?.+?)\\/.*$");
+	private static final Pattern subPathPattern = Pattern.compile("^(\\/contents\\/.+?)\\/.*$");
+    private static final Pattern devicePathPattern = Pattern.compile("^(\\/contents\\/.+?\\/pc|sp)\\/.*$");
 
 	@Override
 	public SessionValueObject getModel(String path) {
@@ -43,7 +44,7 @@ public class ContentsController extends AbstractExternalFileController<SessionVa
 	
 	@Override
 	protected String getTemplate(String path) {
-		String subPath = getSubPath(path);
+        String subPath = getDeviceOrSubPath(path);
 		File template = new File(subPath, "template.ftl");
 		if (getExternalFolderHandler().isExists(template.getPath())) {
 			return String.format("%s/template", subPath);
@@ -82,6 +83,30 @@ public class ContentsController extends AbstractExternalFileController<SessionVa
 		throw new ExternalFileNotFoundException(path);
 	}
 	
+    @RequestMapping(value = "/{sub}/{device:pc|sp}/", method = RequestMethod.GET)
+    public ModelAndView deviceIndex(@PathVariable("sub") String sub, @PathVariable("device") String device, HttpServletRequest req) throws ExternalFileNotFoundException {
+        String fullPath = (String) req.getAttribute(HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE);
+        String bestMatchPattern = (String) req.getAttribute(HandlerMapping.BEST_MATCHING_PATTERN_ATTRIBUTE);
+        String path = String.format("%s/%s/%s",
+                new File(getExternalPathPrefix(), apm.extractPathWithinPattern(bestMatchPattern, fullPath)).getPath(),
+                sub,
+                device);
+        logger().info("sub/device index : " + path + " sub : " + sub + " device : " + device);
+        ExternalFolderHandler handler = getExternalFolderHandler();
+        File template = new File(path, "index.ftl");
+        File html = new File(path, "index.html");
+        File htm = new File(path, "index.htm");
+        if (handler.isExists(template.getPath())) {
+            return new ModelAndView(String.format("%s/%s/%s/index", getExternalPathPrefix(), sub, device), "value", getModel(template.getPath()));
+        } else if (handler.isExists(html.getPath())) {
+            return new ModelAndView("redirect:index.html");
+        } else if (handler.isExists(htm.getPath())) {
+            return new ModelAndView("redirect:index.htm");
+        }
+        logger().info("not supported yet " + path);
+        throw new ExternalFileNotFoundException(path);
+    }
+	
 	private String getSubPath(String path) {
 		Matcher m = subPathPattern.matcher(path);
 		if (m.find()) {
@@ -89,5 +114,12 @@ public class ContentsController extends AbstractExternalFileController<SessionVa
 		}
 		return null;
 	}
-
+	
+	private String getDeviceOrSubPath(String path) {
+	    Matcher dm = devicePathPattern.matcher(path);
+	    if (dm.find()) {
+            return dm.group(1);
+	    }
+	    return getSubPath(path);
+	}
 }
